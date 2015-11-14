8c49685cab718fdc59ffa43e99b7a1c1ab6a28c0
Dave Syer <dsyer@pivotal.io>
2015-11-14 10:30:00 +0000

    Add click link to Facebook

--- src/main/java/com/example/SocialApplication.java
+++ src/main/java/com/example/SocialApplication.java
@@ -15,13 +15,31 @@
  */
 package com.example;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class SocialApplication {
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
+	
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
+				.authenticated();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
