d5579da2278dfd95f35531625b76ec66e06b5be2
Dave Syer <dsyer@pivotal.io>
2016-02-09 15:12:49 +0000

    Add new chapter on custom error page
    Shows how to do some custom authentication and also give users
    feedback if they can't log in.

---
+++ custom-error/src/main/java/com/example/SocialApplication.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@Controller
+public class SocialApplication extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public AuthoritiesExtractor authoritiesExtractor(OAuth2RestOperations template) {
+		return map -> {
+			String url = (String) map.get("organizations_url");
+			@SuppressWarnings("unchecked")
+			List<Map<String, Object>> orgs = template.getForObject(url, List.class);
+			for (Map<String, Object> org : orgs) {
+				if ("spring-projects".equals(org.get("login"))) {
+					return AuthorityUtils
+							.commaSeparatedStringToAuthorityList("ROLE_USER");
+				}
+			}
+			throw new BadCredentialsException("Not in Spring Team");
+		};
+	}
+
+	@RequestMapping("/user")
+	@ResponseBody
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@RequestMapping("/unauthenticated")
+	public String unauthenticated() {
+		return "redirect:/?error=true";
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.antMatcher("/**").authorizeRequests()
+			.antMatchers("/", "/login**", "/webjars/**").permitAll()
+			.anyRequest().authenticated().and()
+			.logout().logoutSuccessUrl("/").permitAll().and()
+			.csrf().csrfTokenRepository(csrfTokenRepository()).and()
+			.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+		// @formatter:on
+	}
+
+	@Configuration
+	protected static class ServletCustomizer {
+		@Bean
+		public EmbeddedServletContainerCustomizer customizer() {
+			return container -> {
+				container.addErrorPages(
+						new ErrorPage(HttpStatus.UNAUTHORIZED, "/unauthenticated"));
+			};
+		}
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+
+	private Filter csrfHeaderFilter() {
+		return new OncePerRequestFilter() {
+			@Override
+			protected void doFilterInternal(HttpServletRequest request,
+					HttpServletResponse response, FilterChain filterChain)
+							throws ServletException, IOException {
+				CsrfToken csrf = (CsrfToken) request
+						.getAttribute(CsrfToken.class.getName());
+				if (csrf != null) {
+					Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+					String token = csrf.getToken();
+					if (cookie == null
+							|| token != null && !token.equals(cookie.getValue())) {
+						cookie = new Cookie("XSRF-TOKEN", token);
+						cookie.setPath("/");
+						response.addCookie(cookie);
+					}
+				}
+				filterChain.doFilter(request, response);
+			}
+		};
+	}
+
+	private CsrfTokenRepository csrfTokenRepository() {
+		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName("X-XSRF-TOKEN");
+		return repository;
+	}
+
+}
