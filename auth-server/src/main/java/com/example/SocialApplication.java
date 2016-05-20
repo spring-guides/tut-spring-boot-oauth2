00e942482734401925f7b3b7466766e3f9c44752
Tobias Schneider <schneider@synyx.de>
2016-05-20 13:21:12 +0200

    Use generic names instead of facebook*

--- auth-server/src/main/java/com/example/SocialApplication.java
+++ auth-server/src/main/java/com/example/SocialApplication.java
@@ -144,14 +144,13 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
 	}
 
 	private Filter ssoFilter(ClientResources client, String path) {
-		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(
+		OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(
 				path);
-		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(client.getClient(),
-				oauth2ClientContext);
-		facebookFilter.setRestTemplate(facebookTemplate);
-		facebookFilter.setTokenServices(new UserInfoTokenServices(
+		OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
+		filter.setRestTemplate(template);
+		filter.setTokenServices(new UserInfoTokenServices(
 				client.getResource().getUserInfoUri(), client.getClient().getClientId()));
-		return facebookFilter;
+		return filter;
 	}
 
 	private Filter csrfHeaderFilter() {
