7db2e7b8d69fe6ab1ea0a5e88139b8395d317085
Dave Syer <dsyer@pivotal.io>
2016-08-01 18:51:38 -0700

    Make changes to code for Boot 1.4 (mostly tests)

--- custom-error/src/main/java/com/example/SocialApplication.java
+++ custom-error/src/main/java/com/example/SocialApplication.java
@@ -15,24 +15,16 @@
  */
 package com.example;
 
-import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -40,16 +32,14 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
 
 @SpringBootApplication
 @EnableOAuth2Sso
@@ -57,13 +47,17 @@ import org.springframework.web.util.WebUtils;
 public class SocialApplication extends WebSecurityConfigurerAdapter {
 
 	@Bean
+	public OAuth2RestTemplate oauth2RestTemplate(OAuth2ProtectedResourceDetails resource, OAuth2ClientContext context) {
+		return new OAuth2RestTemplate(resource, context);
+	}
+
+	@Bean
 	public AuthoritiesExtractor authoritiesExtractor(OAuth2RestOperations template) {
 		return map -> {
 			String url = (String) map.get("organizations_url");
 			@SuppressWarnings("unchecked")
 			List<Map<String, Object>> orgs = template.getForObject(url, List.class);
-			if (orgs.stream()
-					.anyMatch(org -> "spring-projects".equals(org.get("login")))) {
+			if (orgs.stream().anyMatch(org -> "spring-projects".equals(org.get("login")))) {
 				return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
 			}
 			throw new BadCredentialsException("Not in Spring Team");
@@ -84,12 +78,9 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// @formatter:off
-		http.antMatcher("/**").authorizeRequests()
-			.antMatchers("/", "/login**", "/webjars/**").permitAll()
-			.anyRequest().authenticated().and()
-			.logout().logoutSuccessUrl("/").permitAll().and()
-			.csrf().csrfTokenRepository(csrfTokenRepository()).and()
-			.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
+				.authenticated().and().logout().logoutSuccessUrl("/").permitAll().and().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 		// @formatter:on
 	}
 
@@ -98,43 +89,13 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
 		@Bean
 		public EmbeddedServletContainerCustomizer customizer() {
 			return container -> {
-				container.addErrorPages(
-						new ErrorPage(HttpStatus.UNAUTHORIZED, "/unauthenticated"));
+				container.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/unauthenticated"));
 			};
 		}
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
-	}
-
-	private Filter csrfHeaderFilter() {
-		return new OncePerRequestFilter() {
-			@Override
-			protected void doFilterInternal(HttpServletRequest request,
-					HttpServletResponse response, FilterChain filterChain)
-							throws ServletException, IOException {
-				CsrfToken csrf = (CsrfToken) request
-						.getAttribute(CsrfToken.class.getName());
-				if (csrf != null) {
-					Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-					String token = csrf.getToken();
-					if (cookie == null
-							|| token != null && !token.equals(cookie.getValue())) {
-						cookie = new Cookie("XSRF-TOKEN", token);
-						cookie.setPath("/");
-						response.addCookie(cookie);
-					}
-				}
-				filterChain.doFilter(request, response);
-			}
-		};
-	}
-
-	private CsrfTokenRepository csrfTokenRepository() {
-		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-		repository.setHeaderName("X-XSRF-TOKEN");
-		return repository;
 	}
 
 }
--- simple/src/test/java/com/example/SocialApplicationTests.java
+++ simple/src/test/java/com/example/SocialApplicationTests.java
@@ -17,13 +17,11 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SocialApplication.class)
-@WebAppConfiguration
+@SpringBootTest
 public class SocialApplicationTests {
 
 	@Test
