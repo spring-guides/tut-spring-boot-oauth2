6c77314d11ca2782880ccbbd43026ab3bb5962ff
Josh Cummings <josh.cummings@gmail.com>
2019-08-26 13:21:09 -0600

    Update to Spring Security 5.x
    Fixes gh-91

--- simple/src/main/java/com/example/SocialApplication.java
+++ simple/src/main/java/com/example/SocialApplication.java
@@ -17,10 +17,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-@EnableOAuth2Sso
 public class SocialApplication {
 
 	public static void main(String[] args) {
